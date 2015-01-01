12951ec5e0e0b9e41d837f1c12bcc86bf190f670
Tessa <tessakkelly@gmail.com>
2014-12-31 20:03:36 -0700

    amazon scraper add download helper function and function to console log the sales ranking for a book at the given amazon url

--- public/javascripts/scraper.js
+++ public/javascripts/scraper.js
@@ -1,16 +1,34 @@
-//AMAZON:
+// //AMAZON:
 console.log("Scraper JS loaded")
 
-$.ajax({
-  url: "http://www.amazon.com/Yes-Please-Amy-Poehler/dp/0062268341/ref=tmm_hrd_swatch_0?_encoding=UTF8&sr=1-1&qid=1419636038.html",
-  dataType: 'text',
-  success: function(data) {
-    var sales = data.find("#SalesRank").text();
-    var start = sales.indexOf("#");
-    sales = sales.slice(start+1);
-    var end = sales.indexOf(" ");
-    sales = sales.slice(0, end);
-    console.log("Sales Ranking: ")
-    console.log(sales);
-  }
-})
+var amyURL = "http://www.amazon.com/Yes-Please-Amy-Poehler/dp/0062268341/ref=tmm_hrd_swatch_0?_encoding=UTF8&sr=1-1&qid=1419636038.json";
+var grrmURL = "http://www.amazon.com/George-Martins-Thrones-5-Book-Boxed-ebook/dp/B00957T6X6/ref=sr_1_2?s=books&ie=UTF8&qid=1420060001&sr=1-2&keywords=fire+and+ice";
+var bnURL = "http://www.barnesandnoble.com/w/name-of-the-wind-patrick-rothfuss/1100178957?ean=9780756404741";
+
+
+// $.ajax({
+//   url: bnURL,
+//   dataType: 'jsonp',
+//   data: 'data',
+//   xhrFields: {
+//     mozSystem: true
+//   },
+//   success: function(data) {
+//     // console.log("Success")
+//     // var sales = data.find("#SalesRank").text();
+//     // var start = sales.indexOf("#");
+//     // sales = sales.slice(start+1);
+//     // var end = sales.indexOf(" ");
+//     // sales = sales.slice(0, end);
+//     // console.log("Sales Ranking: ")
+//     // console.log(sales);
+//     console.log("Success")
+//     var box = data.find(".product-details box").html();
+//     console.log(box);
+//   },
+//   error: function(one, two, three) {
+//     console.log("Error: " + two);
+//   }
+// })
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var $ = require('jquery')(require('jsdom').jsdom().parentWindow);
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  console.log("rendering index");
   res.render('index', { title: 'BookTracker' });
 });
+
+var url = "http://www.amazon.com/Yes-Please-Amy-Poehler/dp/0062268341/ref=tmm_hrd_swatch_0?_encoding=UTF8&sr=1-1&qid=1419636038";
+var download = function(url, callback) {
+  http.get(url, function(res) {
+  console.log("Got response: " + res.statusCode);
+  var data = "";
+  res.on("data", function(chunk) {
+      data += chunk;
+    });
+
+    res.on("end", function() {
+      callback(data);
+    });
+  }).on("error", function(e) {
+    console.log("Error: " + e.message);
+  });
+}
+
+download(url, function(data) {
+  if (data) {
+    var $data = $($.parseHTML(data));
+    var sales = $data.find("#SalesRank").text();
+    var start = sales.indexOf("#") + 1;
+    sales = sales.slice(start);
+    var end = sales.indexOf(" ");
+    sales = sales.slice(0, end);
+    console.log("Sales Ranking: " + sales);
+  }
+})
 
 module.exports = router;
