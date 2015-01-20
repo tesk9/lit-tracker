fd0fdf675e74c14f928fd70b227e54180c072368
Tessa <tessakkelly@gmail.com>
2015-01-20 13:41:11 -0800

    temporarily disable posting books for heroku deployment

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -27,26 +27,26 @@ var addBook = function() {
     return;
   }
 
-  // errMessage("Adding books disabled. Please try again later.")
+  errMessage("Adding books disabled. Please try again later.")
 
-  $.ajax({
-    url: '/books/new',
-    type: 'POST',
-    data: { name: $title,
-            author: $auth,
-            url: $url,
-            edition: $desc
-          },
-    success: function(r) {
-      var data = JSON.parse(r.book);
-      console.log("Post Book Status: " + r.status)
-      $("#formHolder").prepend("<p>"+data.name+" by "+data.author+" has been added.</p>")
-      $("#new-book-name").val("");
-      $("#new-book-author").val("");
-      $("#new-book-url").val("");
-      $("#new-url-desc").val("");
-    }
-    });
+  // $.ajax({
+  //   url: '/books/new',
+  //   type: 'POST',
+  //   data: { name: $title,
+  //           author: $auth,
+  //           url: $url,
+  //           edition: $desc
+  //         },
+  //   success: function(r) {
+  //     var data = JSON.parse(r.book);
+  //     console.log("Post Book Status: " + r.status)
+  //     $("#formHolder").prepend("<p>"+data.name+" by "+data.author+" has been added.</p>")
+  //     $("#new-book-name").val("");
+  //     $("#new-book-author").val("");
+  //     $("#new-book-url").val("");
+  //     $("#new-url-desc").val("");
+  //   }
+  //   });
 
 };
 
