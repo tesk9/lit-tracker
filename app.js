21a3ad42402ef0c499ff748673c640bc8c960c76
Tessa Kelly <tessakkelly@gmail.com>
2015-05-21 16:12:29 -0700

    refactor scraper into own file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var jade = require('jade');
 var routes = require('./routes/index');
+var amazonScraper = require('./scrapers/amazon.js');
 
 var app = express();
 
---
+++ scrapers/amazon.js
@@ -0,0 +1,59 @@
+var http = require('http');
+var $ = require('jquery')(require('jsdom').jsdom().parentWindow);
+var db = require('../db/bookRankings.js');
+
+module.exports = (function() {
+  // Standardize number format
+  var cleanUpInt = function(numStr) {
+    return numStr.replace(/\,/g, '');
+  };
+
+  // Helper function for scraper
+  var download = function(url, callback) {
+    http.get(url, function(res) {
+    // console.log("Got response: " + res.statusCode);
+    var data = "";
+    res.on("data", function(chunk) {
+        data += chunk;
+      });
+
+      res.on("end", function() {
+        callback(data);
+      });
+    }).on("error", function(e) {
+      console.log("Error: " + e.message);
+    });
+  };
+
+  // Amazon Scraper
+  var getCurrentRankings = function() {
+    db.getAllBooks(function(result) {
+      var books = result;  
+      books.forEach(function(book) {
+        db.getBookURLs({ book_id: book.book_id }, function(urls) {
+          urls.forEach(function(v) {
+            download(v.url, function(data) {
+              scrape(v, data);
+            });
+          })
+        })
+      });
+    });
+  };
+
+  var scrape = function(url, data) {
+    if (data) {
+      var $data = $($.parseHTML(data));
+      var sales = $data.find("#SalesRank").text();
+      var start = sales.indexOf("#") + 1;
+      sales = sales.slice(start);
+      var end = sales.indexOf(" ");
+      sales = sales.slice(0, end);
+      sales = cleanUpInt(sales);
+      var time = new Date();
+      db.addRanking({url_id: url.url_id, ranking: sales, date: time});
+    }
+  };
+
+  setInterval(getCurrentRankings, 200000);
+})();
