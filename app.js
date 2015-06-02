08ac20509b12b7ed4166e2fa9aecb592cc489292
Tessa Kelly <tessakkelly@gmail.com>
2015-06-02 16:19:09 -0700

    restructure bower components and use node in npm start

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -23,6 +22,7 @@ app.use(cookieParser());
 
 app.use('/', routes);
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/bower_components'));
 
 app.disable('etag');
 
