a3d83c7729e2fd7defe0e3e58d165ff101144167
Tessa Kelly <tessakkelly@gmail.com>
2015-06-02 15:22:04 -0700

    refactor form.js to be an angular controller

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,51 +1,38 @@
-$(document).ready(function() {
+App.controller("BookFormController", ["$scope", function($scope) {
+  $scope.newBook = {
+    name: "",
+    author: "",
+    url: "",
+    edition: ""
+  };
+  var emptyForm = angular.copy($scope.newBook);
 
-  // new-book form submit:
-  $("#submit").on('click', function(e) {
-    addBook();
-  });
-  $("#new-book-url").on('keyup', function(e) {
-    if (e.keyCode==13) {
-      addBook();
+  $scope.submit = function() {
+    var newBook = $scope.newBook;
+    if(newBook.name && newBook.author && newBook.url && newBook.edition && newBook.url.match(/^http:\/\/www.amazon.com\//)) {
+      var submission = angular.copy($scope.newBook);
+      clearForm();
+      newMessage(submission.name+" by "+submission.author+" has been added.");
+      $.ajax({
+        url: '/books/new',
+        type: 'POST',
+        data: submission
+      });
+    } else if(!newBook.name || !newBook.author || !newBook.url || !newBook.edition) {
+      newMessage("All fields required")
+    } else {
+      newMessage("Double check your Amazon URL");
     }
-  })
+  };
 
-})
+  var clearForm = function() {
+    $scope.newBook = emptyForm;
+    $scope.addBookForm.$setPristine();
+    newMessage("");
+  };
 
-// verify data existence/types and POST data to server
-var addBook = function() {
-  var $title = $("#new-book-name").val(),
-      $auth = $("#new-book-author").val(),
-      $url = $("#new-book-url").val();
-      $desc = $("#new-url-desc").val()
-  if (!$title || !$auth || !$url || !$desc) {
-    errMessage("All fields required.");
-    return;
-  } else if (!$url.match(/^http:\/\/www.amazon.com\//)) {
-    errMessage("Please input an amazon url.");
-    return;
-  }
+  var newMessage = function(message) {
+    $scope.message = message;
+  };
 
-  $.ajax({
-    url: '/books/new',
-    type: 'POST',
-    data: { name: $title,
-            author: $auth,
-            url: $url,
-            edition: $desc
-          },
-    success: function(r) {
-      var data = JSON.parse(r.book);
-      $("#form-submit-message").html("<span>"+data.name+" by "+data.author+" has been added.</span>")
-      $("#new-book-name").val("");
-      $("#new-book-author").val("");
-      $("#new-book-url").val("");
-      $("#new-url-desc").val("");
-    }
-  });
-
-};
-
-var errMessage = function(err) {
-  $("#form-submit-message").html("<span class='text-error'> "+err+"</span>");
-};
+}]);
