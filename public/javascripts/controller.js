aa85a4712c598798c425a873ff79598d74619258
Tessa Kelly <tessakkelly@gmail.com>
2015-06-02 21:45:32 -0700

    refactor rankings display to use ng-repeat

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,6 +1,16 @@
-Query.getAllBooks(function(book) {
-  View.labelSvg(book);  
-  Query.getBookRankings(book, function(data, bookID) {
-    View.callGraph(data, bookID);
+App.controller("graphController", ["$scope", "$q", function($scope, $q) {
+  //TODO: Query.getAllBooks should return a promise
+  $q(Query.getAllBooks()).then(function(books) {
+    $scope.books = books;
+
+    books.forEach(function(book) {
+      Query.getBookRankings(book, function(data, bookID) {
+        View.callGraph(data, bookID);
+      });
+    });
+    
   });
-});
+}]);
+
+
+
--- public/javascripts/model.js
+++ public/javascripts/model.js
@@ -1,14 +1,23 @@
 var Query = (function() {
+  // var getAllBooks = function(callback) {
+  //   $.get('/books', function(books) {
+  //     var books = JSON.parse(books.books);
+  //     books.forEach(function(v) {
+  //       if(callback) { 
+  //         callback(v);
+  //       }
+  //     })
+  //   })
+  // }
+
   var getAllBooks = function(callback) {
-    $.get('/books', function(books) {
-      var books = JSON.parse(books.books);
-      books.forEach(function(v) {
-        if(callback) { 
-          callback(v);
-        }
-      })
-    })
-  }
+    return function(resolve, reject) {
+      $.get('/books', function(books) {
+        var books = JSON.parse(books.books);
+        resolve(books);
+      });
+    };
+  };
 
   var getBookRankings = function(book, callback) {
     $.get('/books/' + book.book_id, function(data) {
