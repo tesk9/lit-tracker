dec6df0f58f024ebd5a11408ebc7c4ed6210a455
Tessa Kelly <tessakkelly@gmail.com>
2015-07-01 11:22:33 -0700

    make add new edition form hideable/showable on title click

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,7 +1,7 @@
 App.controller("graphController", ["$scope", "queries", "searchTerm", "grapher", function($scope, queries, searchTerm, grapher) {
   queries.getAllBooks().then(function(books) {
     $scope.books = books;
-
+    $scope.addNewEdition = false;
     books.forEach(function(book) {
       queries.getBookRankings(book, function(data, bookID) {
         grapher(data, bookID);
